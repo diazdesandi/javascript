202d27f242b52f87243d58e12616d5390e76fff2
René <diaz.de.sandi.7@gmail.com>
2022-04-16 18:37:13 -0700

    114. Looping Objects: Object Keys, Values, and Entries

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -51,34 +51,57 @@ const restaurant = {
   },
 };
 
+/////////// Looping Objects: Object Keys, Values, and Entries ///////////
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Loop through the object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
 /////////// Optional Chaining (?.) ///////////
-if (restaurant.openingHours && restaurant.openingHours.mon) {
-  console.log(restaurant.openingHours.mon.open);
-}
+// if (restaurant.openingHours && restaurant.openingHours.mon) {
+//   console.log(restaurant.openingHours.mon.open);
+// }
 
-// With optional chaining:
-// Only work if openingHours is defined and openingHours.mon is defined,
-// otherwise return undefined
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);
+// // With optional chaining:
+// // Only work if openingHours is defined and openingHours.mon is defined,
+// // otherwise return undefined
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
 
-// Example
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// // Example
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open || 'closed';
-  console.log(`On ${day} we open at ${open}`);
-}
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open || 'closed';
+//   console.log(`On ${day} we open at ${open}`);
+// }
 
-// Methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// // Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
-// Arrays
-// const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// // Arrays
+// // const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
 
-const users = [];
-console.log(users[0]?.name ?? 'User array empty');
+// const users = [];
+// console.log(users[0]?.name ?? 'User array empty');
 
 /////////// Looping Arrays: The for-of Loop ///////////
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
