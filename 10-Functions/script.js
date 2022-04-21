935873533ce58361dadbcac44f423231c12a38ad
René <diaz.de.sandi.7@gmail.com>
2022-04-21 00:24:24 -0700

    138. More Closure Examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -334,3 +334,43 @@ A closure gives a function access to all the variables of it's parent function,
 even after that parent function has returned. The function keeps a reference to 
 it's outer scope, which preserves the scope chain throughout time.
 */
+
+//////////////////// More Closure Examples ////////////////////
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
