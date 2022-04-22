1f060e49501d960386a1ea4fc67b131568b4bb36
René <diaz.de.sandi.7@gmail.com>
2022-04-21 18:51:37 -0700

    144. Looping Arrays: forEach

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -110,13 +110,42 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
 // 143. The new at Method
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.at(0));
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
 
-// getting last array element
-console.log(arr[arr.length - 1]); // last element of any array
-console.log(arr.slice(-1)[0]); // last element of any array
-console.log(arr.at(-1)); // last element of any array
+// // getting last array element
+// console.log(arr[arr.length - 1]); // last element of any array
+// console.log(arr.slice(-1)[0]); // last element of any array
+// console.log(arr.at(-1)); // last element of any array
 
-console.log('jonas'.at(0)); // j
+// console.log('jonas'.at(0)); // j
+
+/////////////////////////////////////////////////
+
+// 144. Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited $${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew $${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited $${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew $${Math.abs(mov)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// Until the end of the array
+// You cannot break out of the forEach loop.
