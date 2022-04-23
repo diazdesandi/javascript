47cbe0e1307e0667e4465ba8e09b5a091cdb6b76
René <diaz.de.sandi.7@gmail.com>
2022-04-22 17:35:03 -0700

    151. Computing Usernames

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -81,6 +81,22 @@ const displayMovements = function (movements) {
 displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
+
+// 151. Computing Usernames
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map((name) => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
+
+/////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
@@ -235,35 +251,35 @@ GOOD LUCK 😀
 /////////////////////////////////////////////////
 
 // 150. The map method.
-const eurToUsd = 1.1;
+// const eurToUsd = 1.1;
 
-const movementsUSD = movements.map(function (mov) {
-  return mov * eurToUsd;
-});
-
-// const movementsUSD = movements.map((mov) => mov * eurToUsd);
-
-console.log(movements);
-console.log(movementsUSD);
-
-const movementsUSDfor = [];
-for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-console.log(movementsUSDfor);
-
-// const movementsDescriptions = movements.map((mov, i, arr) => {
-//   if (mov > 0) {
-//     console.log(`Movement ${i + 1}: You deposited $${mov}`);
-//   } else {
-//     console.log(`Movement ${i + 1}: You withdrew $${Math.abs(mov)}`);
-//   }
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
 // });
 
-// Simplified version
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} $${Math.abs(
-      mov
-    )}`
-);
+// // const movementsUSD = movements.map((mov) => mov * eurToUsd);
 
-console.log(movementsDescriptions);
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
+
+// // const movementsDescriptions = movements.map((mov, i, arr) => {
+// //   if (mov > 0) {
+// //     console.log(`Movement ${i + 1}: You deposited $${mov}`);
+// //   } else {
+// //     console.log(`Movement ${i + 1}: You withdrew $${Math.abs(mov)}`);
+// //   }
+// // });
+
+// // Simplified version
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} $${Math.abs(
+//       mov
+//     )}`
+// );
+
+// console.log(movementsDescriptions);
