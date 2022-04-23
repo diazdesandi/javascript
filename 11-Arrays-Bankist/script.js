7e6deed2711387cbf8f75ca07e9245fd1b10bcf7
René <diaz.de.sandi.7@gmail.com>
2022-04-22 20:24:20 -0700

    FIXED: 156. Coding Challenge #3

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -433,6 +433,13 @@ const calcAverageHumanAge = (ages) => {
   return average;
 };
 
+// Optimal solution:
+// const calcAverageHumanAge = ages =>
+//   ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
 const testData1 = [5, 2, 4, 1, 15, 8, 3];
 const testData2 = [16, 6, 10, 5, 6, 1, 4];
 
