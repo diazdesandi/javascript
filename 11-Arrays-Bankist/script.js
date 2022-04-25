6781f4840a12377ef6fa09924843bda73a68a4e7
René <diaz.de.sandi.7@gmail.com>
2022-04-25 01:41:32 -0700

    164. More Ways of Creating and Filling Arrays

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -626,39 +626,73 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 // 163. Sorting Arrays
 
 // Strings
-const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
-console.log(owners.sort());
-console.log(owners);
+// const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+// console.log(owners.sort());
+// console.log(owners);
 
-// Numbers
-console.log(movements);
+// // Numbers
+// console.log(movements);
 
-// return < 0, A, B (keep order)
-// return > 0, B, A (switch order)
-// Ascending order
-// Old version
-// movements.sort((a, b) => {
-//   if (a > b) {
-//     return 1;
-//   }
-//   if (b > a) {
-//     return -1;
-//   }
-// });
-// Improved version
-movements.sort((a, b) => a - b);
-console.log(movements);
+// // return < 0, A, B (keep order)
+// // return > 0, B, A (switch order)
+// // Ascending order
+// // Old version
+// // movements.sort((a, b) => {
+// //   if (a > b) {
+// //     return 1;
+// //   }
+// //   if (b > a) {
+// //     return -1;
+// //   }
+// // });
+// // Improved version
+// movements.sort((a, b) => a - b);
+// console.log(movements);
 
-// Descending order
-// Old version
-// movements.sort((a, b) => {
-//   if (a > b) {
-//     return -1;
-//   }
-//   if (b > a) {
-//     return 1;
-//   }
-// });
-// Improved version
-movements.sort((a, b) => b - a);
-console.log(movements);
+// // Descending order
+// // Old version
+// // movements.sort((a, b) => {
+// //   if (a > b) {
+// //     return -1;
+// //   }
+// //   if (b > a) {
+// //     return 1;
+// //   }
+// // });
+// // Improved version
+// movements.sort((a, b) => b - a);
+// console.log(movements);
+
+/////////////////////////////////////////////////
+
+// 164. More Ways of Creating and Filling Arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+const x = new Array(7); // Creates an array of 7 empty elements
+console.log(x);
+console.log(x.map(() => 5)); // Nothing happens, because the array is empty
+
+// x.fill(1); // Fills the array with 1
+// x.fill(1, 3); // Fills the array with 1 from index 3
+x.fill(1, 3, 5); // Fills the array with 1 from index 3 to index 5
+console.log(x);
+
+arr.fill(23, 2, 6); // Fills the array with 23 from index 2 to index 6
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    (el) => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
