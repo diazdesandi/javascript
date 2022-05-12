03a5b722104cd79a19b4c65cc512f6a57c201109
René <diaz.de.sandi.7@gmail.com>
2022-05-11 17:32:49 -0700

    218. Inheritance Between "Classes": Constructor Functions

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -245,37 +245,79 @@ GOOD LUCK 😀
 */
 
 // 1.
-class CarCl {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelerate() {
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-  }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+//   }
 
-  brake() {
-    this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-  }
-  // 2.
-  get speedUS() {
-    return this.speed / 1.6;
-  }
-  // 3.
-  set speedUS(speed) {
-    this.speed = speed * 1.6;
-  }
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+//   }
+//   // 2.
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+//   // 3.
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+
+// // 4.
+// const ford = new CarCl('Ford', 120);
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.accelerate();
+
+// ford.speedUS = 50;
+// console.log(ford.speedUS);
+
+// 218. Inheritance Between "Classes": Constructor Functions
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
 }
 
-// 4.
-const ford = new CarCl('Ford', 120);
-ford.accelerate();
-ford.accelerate();
-ford.brake();
-ford.accelerate();
+const Student = function (firstName, birthYear, course) {
+  // this.firstName = firstName;
+  // this.birthYear = birthYear;
+  // Instead of using the above, we use: 
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+}
 
-ford.speedUS = 50;
-console.log(ford.speedUS);
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype = Person.prototype; // Doesn't work.
+
+Student.prototype.introduce = function () {
+  console.log(`Hi, my name is ${this.firstName} and I'm a ${this.course} student.`);
+}
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person); // Because Student and Person are linked to each other.
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
