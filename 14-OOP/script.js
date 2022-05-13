35eed7bbab4c1f6b839bac15ae75e8c5fcb062b1
René <diaz.de.sandi.7@gmail.com>
2022-05-12 19:08:21 -0700

    221. Inheritance Between "Classes": Object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -376,62 +376,91 @@ GOOD LUCK 😀
 
 
 // 220. Inheritance Between "Classes": ES6 Classes
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hey ${this.firstName}`);
+//   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     console.log(name);
+//     if (name.includes(' ')) {
+//       this._fullName = name;
+//     } else {
+//       alert(`${name} is not a full name.`);
+//     }
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   static hey() {
+//     console.log('Hey there!');
+//     console.log(this);
+//   }
+// }
+
+// // Make a new class called Student that inherits from Person.
+// class StudentCl extends PersonCl {
+//   constructor(fullName, birthYear, course) {
+//     // Always call the parent constructor first.
+//     // Creates the this keyword.
+//     super(fullName, birthYear); // Constructor of the parent class.
+//     this.course = course;
+//   }
+
+//   introduce() {
+//     console.log(`Hi, my name is ${this.fullName} and I'm a ${this.course} student.`);
+//   }
+
+//   // Overriding the parent method.
+//   calcAge() {
+//     console.log(`I'm ${2037 - this.birthYear} years old, but as a student I feel more like ${2037 - this.birthYear + 10}`);
+//   }
+// }
+
+// const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+// martha.introduce();
+// martha.calcAge();
+
+// 221. Inheritance Between "Classes": Object.create
+const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
-  }
+  },
 
-  greet() {
-    console.log(`Hey ${this.firstName}`);
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
   }
+};
 
-  get age() {
-    return 2037 - this.birthYear;
-  }
+const steven = Object.create(PersonProto);
 
-  set fullName(name) {
-    console.log(name);
-    if (name.includes(' ')) {
-      this._fullName = name;
-    } else {
-      alert(`${name} is not a full name.`);
-    }
-  }
-
-  get fullName() {
-    return this._fullName;
-  }
-
-  static hey() {
-    console.log('Hey there!');
-    console.log(this);
-  }
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
 }
 
-// Make a new class called Student that inherits from Person.
-class StudentCl extends PersonCl {
-  constructor(fullName, birthYear, course) {
-    // Always call the parent constructor first.
-    // Creates the this keyword.
-    super(fullName, birthYear); // Constructor of the parent class.
-    this.course = course;
-  }
-
-  introduce() {
-    console.log(`Hi, my name is ${this.fullName} and I'm a ${this.course} student.`);
-  }
-
-  // Overriding the parent method.
-  calcAge() {
-    console.log(`I'm ${2037 - this.birthYear} years old, but as a student I feel more like ${2037 - this.birthYear + 10}`);
-  }
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
 }
 
-const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
-martha.introduce();
-martha.calcAge();
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
